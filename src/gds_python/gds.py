9fb585a48a97a4a8613f750aa9ecc4c3cdad2401
Moxious <moxious@oldhat.org>
2021-05-04 09:36:42 -0400

    mod

--- src/gds_python/gds.py
+++ src/gds_python/gds.py
@@ -8,6 +8,8 @@ class GDS:
 
     def connect(self):
         """Call this method to verify connectivity to the Neo4j system, and to bootstrap the API"""
+        print("Generating GDS API")
         self.generator = APIGenerator(self.driver)
         self.api = self.generator.generate()
+        print("Constructing GDS API")
         return GDSAPI(self.api, self.driver)
