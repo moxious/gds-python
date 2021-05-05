cb573f982bb8c36385d5dd203276cf398edd172a
Moxious <moxious@oldhat.org>
2021-05-05 09:13:21 -0400

    driver kwargs

--- src/gds_python/gds.py
+++ src/gds_python/gds.py
@@ -3,8 +3,8 @@ from neo4j import GraphDatabase
 from gds_python import GDSAPI
 
 class GDS:
-    def __init__(self, url, username, password):
-        self.driver = GraphDatabase.driver(url, auth=(username, password))
+    def __init__(self, url, username, password, **driver_kwargs):
+        self.driver = GraphDatabase.driver(url, auth=(username, password), **driver_kwargs)
 
     def connect(self):
         """Call this method to verify connectivity to the Neo4j system, and to bootstrap the API"""
